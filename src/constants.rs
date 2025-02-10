420eb8cc19aec7383908d14efc0a26a2abf90d29
Eric Park <me@ericswpark.com>
2025-02-10 12:58:26 -0500

    Change session timeout and addition to 15 minutes
    We still use two separate constants in case we want to customize this in
    the future.

--- src/constants.rs
+++ src/constants.rs
@@ -6,9 +6,9 @@
 /// specific session, as well as the session start minutes defined by this constant. This is done
 /// as we do not know when the user has started working on a specific coding session, and therefore
 /// we have to add based on a guess.
-pub const SESSION_START_ADDITION_IN_MINUTES: usize = 120;
+pub const SESSION_START_ADDITION_IN_MINUTES: usize = 15;
 
 /// Minutes the user can idle before the session is considered to be separate
 ///
 /// If no commits are made for the specified duration, the next commit starts a new coding session.
-pub const MAX_SESSION_IDLE_IN_MINUTES: usize = 120;
+pub const MAX_SESSION_IDLE_IN_MINUTES: usize = SESSION_START_ADDITION_IN_MINUTES;

b02bea69108868b061f37c29b21980d7d537734f
Eric Park <me@ericswpark.com>
2025-02-10 11:01:53 -0500

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+    description = "Flake for Rust";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {self, nixpkgs, flake-utils, ... }@inputs:
+        flake-utils.lib.eachDefaultSystem (
+            system:
+            let
+                overlays = [ ];
+                pkgs = import nixpkgs { inherit system overlays; };
+            in
+            with pkgs;
+            {
+                devShells.default = mkShell {
+                    nativeBuildInputs = [
+                        cargo
+                        rustc
+                        gcc
+                        rustfmt
+                        clippy
+                    ];
+                    RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+                };
+            }
+        );
+}

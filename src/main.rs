5500d9bc5624970576be13d59bbf40556814e5f8
Eric Park <me@ericswpark.com>
2025-02-10 13:27:58 -0500

    Rearrange main and add comments

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,20 @@ fn main() {
     // Attempt to open repository in current directory, or start walking up
     let repo = get_repository();
     let initial_commit = get_initial_commit(&repo).expect("Failed to get initial commit");
-    let commit_counts = get_commit_counts(&repo).expect("Failed to get commit counts");
-    let estimates = get_estimate_minutes(&repo).expect("Failed to get estimate minutes");
-    let metadata = zip_by_author(commit_counts, estimates);
 
-    // Fetch metadata from repository
+    // Fetch initial commit time from repository
     let initial_commit_time_raw = initial_commit.time().seconds();
     let initial_commit_time = get_localized_time(initial_commit_time_raw).unwrap();
-
     println!(
         "Initial commit was made at {} ({})",
         get_formatted_time(initial_commit_time),
         get_humanized_time(initial_commit_time)
     );
 
+    // Fetch author metadata (commit count, total session duration) from repository
+    let commit_counts = get_commit_counts(&repo).expect("Failed to get commit counts");
+    let estimates = get_estimate_minutes(&repo).expect("Failed to get estimate minutes");
+    let metadata = zip_by_author(commit_counts, estimates);
     for entry in metadata {
         println!(
             "{}: {} commits ({})",

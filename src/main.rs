f5df4a070e14f2f8df4b1d04c2c6e44578cddafa
Eric Park <me@ericswpark.com>
2025-02-24 14:21:11 -0500

    Move library functions to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use anyhow::{Context, Result};
 use clap::Parser;
-use cs252chkr::constants::{INSERTION_DELETION_WARNING_RATIO, SENTRY_DSN_URL};
+use cs252chkr::constants::SENTRY_DSN_URL;
 use cs252chkr::*;
 use sentry::ClientInitGuard;
-use std::collections::HashMap;
 
 #[derive(Parser, Debug)]
 #[command(name = "cs252chkr")]
@@ -48,67 +47,6 @@ fn main() -> Result<()> {
     }
 
     Ok(())
-}
-
-/// Formats and prints commit statistics
-///
-/// Warning: it is up to the caller to make sure that the stat and time objects belong to the same
-/// author!
-///
-/// * `stat` - commit statistics to print
-/// * `time` - time spent information for author
-fn print_commit_stats(stat: &CommitStats, time: &CommitTime) {
-    // Pretty-print author name in border
-    for _ in 0..stat.author().len() + 4 {
-        print!("=");
-    }
-    println!();
-    println!("| {} |", stat.author());
-    for _ in 0..stat.author().len() + 4 {
-        print!("=");
-    }
-    println!();
-
-    // Print basic commit stats
-    println!("Commits: {} commits", stat.count);
-    println!("Time spent: {}", get_humanized_minutes(time.time as i64));
-
-    // Calculate ratio of insertions and deletions and print LOC stats
-    let ratio = stat.deletions as f64 / stat.insertions as f64;
-    print!(
-        "LOC: +{}/-{} ({:.2}%)",
-        stat.insertions,
-        stat.deletions,
-        ratio * 100.0
-    );
-
-    // Warn if LOC ratio is strangely low
-    if ratio < INSERTION_DELETION_WARNING_RATIO {
-        println!(" (!)");
-    } else {
-        println!();
-    }
-}
-
-/// Zip two arrays of CommitMetadata objects into tuples for easy access
-///
-/// * `vec1` - Vec of objects that implement the CommitMetadata trait
-/// * `vec2` - Vec of objects that implement the CommitMetadata trait
-fn zip_by_author<U, V>(vec1: Vec<U>, vec2: Vec<V>) -> Vec<(String, (U, V))>
-where
-    U: CommitMetadata,
-    V: CommitMetadata,
-{
-    let mut map: HashMap<String, U> = vec1.into_iter().map(|x| (x.author(), x)).collect();
-    let mut result = Vec::new();
-
-    for val2 in vec2 {
-        if let Some(val1) = map.remove(&val2.author()) {
-            result.push((val1.author(), (val1, val2)));
-        }
-    }
-
-    result
 }
 
 /// Initializes Sentry, a bug report platform
